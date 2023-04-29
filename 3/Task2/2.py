57e045646edc48990b7e0c5e7d2f3dd340107d09
Ana Shevchenko <shevchenko_a@list.ru>
2023-04-29 16:14:40 +0700

    Homework3

---
+++ 3/Task2/2.py
@@ -0,0 +1,24 @@
+# Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент
+# к заданному числу X. Пользователь в первой строке вводит натуральное число N – 
+# количество элементов в массиве. В последующих  строках записаны N целых чисел Ai. 
+# Последняя строка содержит число X
+
+# *Пример:*
+
+# 5
+#     1 2 3 4 5
+#     6
+#     -> 5
+
+massive_size = int(input("Введите количество целых чисел "))
+list_1 = [i + 1 for i in range(massive_size)]
+print(list_1)
+number_to_find = int(input("Введите число для поиска "))
+
+for i in list_1:
+    if list_1[i - 1] == number_to_find:
+        print(number_to_find)
+if number_to_find < list_1[0]:
+    print(list_1[0])
+elif number_to_find > list_1[massive_size - 1]:
+    print(list_1[massive_size - 1])
